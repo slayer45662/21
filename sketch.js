47c9be2086ce98059c3911c22805e8cded1bbd24
slayer45662 <78002266+slayer45662@users.noreply.github.com>
2021-02-16 20:32:31 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var fixedRect, movingRect;
+var joe
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  joe = createSprite(400,300,20,20)
+  joe.shapeColor = "blue"
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,joe)){
+    movingRect.shapeColor = "red";
+    joe.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    joe.shapeColor = "blue";
+  }
+  drawSprites();
+}
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+      return true
+    }
+    else{
+      return false
+    }
+      
+      
+}
